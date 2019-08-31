ba57ebe4f6d28aa39890afd84baf97c8e2011f5a
edisonbedoya98 <edison.bedoyag@udea.edu.co>
2019-08-31 12:46:21 -0500

    update

--- cypress/integration/codebreacker.spec.js
+++ cypress/integration/codebreacker.spec.js
@@ -1,12 +1,6 @@
 describe('My first e2e testing', function(){
     it('Check website', function(){
-         //Arrange
-         // - visit some place
-         cy.visit('http://localhost:3001/')
-         //Act
-         // Search a element y click
-         //cy.contains('set').click()
-         //Assert       
+         cy.visit('http://localhost:3001/')           
          cy.get('#numero')
              .type('1234')
              .should('have.value','1234')
@@ -21,25 +15,5 @@ describe('My first e2e testing', function(){
 
           
      })
-   /*  it('Search an Iphone',function(){
-         cy.visit('http://localhost:4200/')
-         cy.contains('Estefany')
-         cy.get('input')
-             .type('iphone 8 plus 64gb')
-             .should('have.value','iphone 8 plus 64gb')
-         cy.get('button')
-             .click()
-         cy.contains('iPhone')
- 
-         cy.get('input')
-             .clear()
-             .type('Nikon 750')
-             .should('have.value','Nikon 750')
-         cy.get('button')
-         .click()
-         cy.contains('Nikon')
- 
-     
-     })*/
  
  })
